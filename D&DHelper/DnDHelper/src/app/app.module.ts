b51bbe271b77605ae42cb3161b07049981ec94df
ChristopheBleiman <christophe.bleiman@student.odisee.be>
2021-11-06 12:13:04 +0100

    fix nav + start compendium

--- D&DHelper/DnDHelper/src/app/app.module.ts
+++ D&DHelper/DnDHelper/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DicerollerComponent } from './diceroller/diceroller.component';
@@ -9,21 +9,26 @@ import {ButtonModule} from 'primeng/button';
 import {ToolbarModule} from 'primeng/toolbar';
 import {TabMenuModule} from 'primeng/tabmenu';
 import { TabNavComponent } from './tab-nav/tab-nav.component';
+import { CompendiumComponent } from './compendium/compendium.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      { path: '', redirectTo: '/compendium', pathMatch: 'full' },
+      { path: 'compendium', component: CompendiumComponent },
       { path: 'diceroller', component: DicerollerComponent },
     ]),
     ButtonModule,
     ToolbarModule,
     TabMenuModule,
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
     DicerollerComponent,
     TabNavComponent,
+      CompendiumComponent
    ],
   providers: [],
   bootstrap: [AppComponent]
