c53e804f651c986c1e12d3c7f2ed39d0b7f60300
ChristopheBleiman <christophe.bleiman@student.odisee.be>
2021-11-18 10:39:08 +0100

    DEATH TO API

--- D&DHelper/DnDHelper/src/app/spells.service.ts
+++ D&DHelper/DnDHelper/src/app/spells.service.ts
@@ -9,8 +9,17 @@ export class SpellsService {
 
   constructor(private http:HttpClient) { }
 
-  getData()  {
+  getData() {
     let url = "https://www.dnd5eapi.co/api/spells/";
-    return this.http.get(url);
+    return this.http.get<SpellList>(url);
   }
+}
+export interface Spell{
+  index: string;
+  name: string;
+  url: string;
+}
+export interface SpellList{
+  count: any;
+  results: Spell[];
 }
